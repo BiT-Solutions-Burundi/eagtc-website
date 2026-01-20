a09cd30d38d40bec171c486acb320c08e3b8e4c8
VanGoethe <iy.vangoethe@gmail.com>
2026-01-20 18:46:43 +0200

    Make feature cards full width on mobile (1 column), 2 columns on small screens and up

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -89,7 +89,7 @@ export default function WhyChooseUs() {
               </div>
             </div>
 
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
               {features.map((feature, index) => (
                 <div
                   key={feature.title}
